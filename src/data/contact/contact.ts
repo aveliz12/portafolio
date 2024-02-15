76d3d85b82b1f491345f1add6c9590c8b8e2646a
apveliz <apvelizs@utn.edu.ec>
2024-02-14 22:09:09 -0500

    Feat: Página de Contact

--- src/data/contact/contact.ts
+++ src/data/contact/contact.ts
@@ -1,6 +1,6 @@
 export const contact = [
   {
-    location: "Sede en Contacachi,Imbabura,Ecuador",
+    location: "Sede en Contacachi, Imbabura, Ecuador.",
     telf: "+593 98 272 5478",
     email: "alexpaulveliz12@gmail.com",
   },
--- src/pages/portafolio/Contact.tsx
+++ src/pages/portafolio/Contact.tsx
@@ -1,10 +1,38 @@
 import { Header } from "../../components/components";
+import { contact } from "../../data/contact/contact";
+import phone from "../../assets/phone.png";
+import location from "../../assets/location.png";
+import email from "../../assets/email.png";
 import "../../styles/contact_styles.css";
 const Contact = () => {
   return (
     <div style={{ minHeight: "100vh" }}>
       <Header />
-      <h1>CONTACTO</h1>
+      <div className="title-contact">
+        <h1>Contacto</h1>
+      </div>
+      {contact.map((data) => (
+        <div className="container-contact" key={1}>
+          <div data-text="Teléfono" className="glass">
+            <div className="glass-container">
+              <img src={phone} alt="" width={60} height={60} />
+              <p className="text-contact">{data.telf}</p>
+            </div>
+          </div>
+          <div data-text="Ubicación" className="glass">
+            <div className="glass-container">
+              <img src={location} alt="" width={60} height={60} />
+              <p className="text-contact">{data.location}</p>
+            </div>
+          </div>
+          <div data-text="Correo electrónico" className="glass">
+            <div className="glass-container">
+              <img src={email} alt="" width={60} height={60} />
+              <p className="text-contact">{data.email}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
